efceb8afa94fa10f8331a257d15ff9c0119f2775
Muhammad Zeeshan Ahsan <ahsansahi6@gmail.com>
2022-08-23 14:18:24 +0500

    setup

---
+++ resources/views/includes/admin-header.blade.php
@@ -0,0 +1,10 @@
+<!-- icons cdn -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- ============== Font Awesome ============== -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+<!-- ============== Bootstrap Css Links =============== -->
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/bootstrap.min.css')}}">
+<!-- ============== Custom Css Link ============== -->
+<link rel="stylesheet" href="{{asset('assets/css/zeeshan.css')}}">
+<link rel="stylesheet" href="{{asset('assets/css/tahira.css')}}">
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login');
 });
 
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
