754182b40697baa38594caab3329d77bc2aa8948
atifkodex <atif.kodex@gmail.com>
2022-08-25 12:41:54 +0500

    amin_topup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,9 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->string('country')->nullable();
+            $table->string('date_of_birth')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
