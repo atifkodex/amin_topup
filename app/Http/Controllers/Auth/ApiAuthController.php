e0fe8156b99753d50c0767f137dd1ff98ccefae7
Wahid Ahmad <wahid.a@kodextech.com>
2022-08-30 10:53:35 +0500

    Update ApiAuthController.php

--- app/Http/Controllers/Auth/ApiAuthController.php
+++ app/Http/Controllers/Auth/ApiAuthController.php
@@ -107,13 +107,13 @@ class ApiAuthController extends Controller
         }elseif(isset($request->email) && !empty($request->email)){
             $user = User::where('id', $request->id)->update(['email' => $request->email]);
         }elseif(isset($request->phone_number) && !empty($request->phone_number)){
-            $user = User::where('id', $request->id)->update(['email' => $request->phone_number]);
+            $user = User::where('id', $request->id)->update(['phone_number' => $request->phone_number]);
         }elseif(isset($request->date_of_birth) && !empty($request->date_of_birth)){
-            $user = User::where('id', $request->id)->update(['email' => $request->date_of_birth]);
+            $user = User::where('id', $request->id)->update(['date_of_birth' => $request->date_of_birth]);
         }elseif(isset($request->profile) && !empty($request->profile)){
-            $user = User::where('id', $request->id)->update(['email' => $request->profile]);
+            $user = User::where('id', $request->id)->update(['profile' => $request->profile]);
         }elseif(isset($request->country) && !empty($request->country)){
-            $user = User::where('id', $request->id)->update(['email' => $request->country]);
+            $user = User::where('id', $request->id)->update(['country' => $request->country]);
         }else{
             return $this->sendError("At least one parameter must be provided.");
         }
