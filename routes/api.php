0a15423d2572eea78be9da6a7000e72663d3a514
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-06 17:34:57 +0500

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -52,7 +52,6 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/notification_list', [SettingController::class, 'notificationList']);
     Route::post('/image_link', [SettingController::class, 'createImageLink']);
     Route::post('/user_profile', [UserController::class, 'userProfile']);
-    Route::post('/reply_send', [AdminController::class, 'replySend'])->name('reply_send.api');
 });
 
 Route::post('/send_otp', [OtpController::class, 'sendOTP']);
