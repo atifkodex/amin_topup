153e49dddeda6908aa66cfa1086aff30d641e76a
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-08 19:06:25 +0500

    update

--- routes/api.php
+++ routes/api.php
@@ -39,21 +39,21 @@ Route::group(['middleware' => ['cors', 'json.response']], function () {
     Route::post('/register', [ApiAuthController::class, 'register'])->name('register.api');
 });
 
-Route::middleware([IsAdmin::class])->group(function () {
-    // Route::group(['prefix' => 'admin', 'middleware' => ['cors', 'json.response']], function () {
-        Route::get('/users_list', [UserController::class, 'allUsers']);
-        Route::post('/check_operator', [UserController::class, 'networkOperator']);
-        Route::post('/add_operator_data', [OperatorController::class, 'operatorData']);
-        Route::post('/support', [ContactsController::class, 'user_support'])->name('support.api');
-        Route::post('/create_admin', [AdminController::class, 'create_admin'])->name('create_admin.api');
-        Route::post('/users', [AdminController::class, 'usersList'])->name('users.api');
-        Route::post('/dashboard', [AdminController::class, 'adminDashboard']);
-        Route::post('/settings', [SettingController::class, 'settingsData']);
-        Route::post('/reply_send', [AdminController::class, 'replySend'])->name('reply_send.api');
-        Route::post('/update_operator', [SettingController::class, 'updateOperator']);
-    // });
-});
 Route::middleware('auth:api')->group(function () {
+    // Route::middleware([IsAdmin::class])->group(function () {
+        // Route::group(['prefix' => 'admin', 'middleware' => ['cors', 'json.response']], function () {
+            Route::get('/users_list', [UserController::class, 'allUsers']);
+            Route::post('/check_operator', [UserController::class, 'networkOperator']);
+            Route::post('/add_operator_data', [OperatorController::class, 'operatorData']);
+            Route::post('/support', [ContactsController::class, 'user_support'])->name('support.api');
+            Route::post('/create_admin', [AdminController::class, 'create_admin'])->name('create_admin.api');
+            Route::post('/users', [AdminController::class, 'usersList'])->name('users.api');
+            Route::post('/dashboard', [AdminController::class, 'adminDashboard']);
+            Route::post('/settings', [SettingController::class, 'settingsData']);
+            Route::post('/reply_send', [AdminController::class, 'replySend'])->name('reply_send.api');
+            Route::post('/update_operator', [SettingController::class, 'updateOperator']);
+        // });
+    // });
 
     Route::get('/articles', [ArticleController::class, 'index'])->middleware('api.admin')->name('articles');
     Route::post('/logout', [ApiAuthController::class, 'logout'])->name('logout.api');
