0498b2240c6ee0a221a787dd78cf6457eae375a9
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-09 17:21:36 +0500

    Revert "Merge branch 'Zeeshan'"
    This reverts commit bcbcef85c14af0f61deb8cd9a99bb77e5d22b870, reversing
    changes made to eccf556a74bbd2a3ae4e85260d4b180e73d00acc.

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -32,11 +32,6 @@
         background: #f1efef;
 
     }
-    @media screen and (min-width:1700px) {
-     #table-id{
-        width: 100% !important;
-     }
-    }
 </style>
 @section('content')
     @include('includes.admin-navbar')
