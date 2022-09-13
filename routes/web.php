51f69525ba202cfab2e9951e507443806cec3305
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-13 11:57:22 +0500

    update

--- routes/web.php
+++ routes/web.php
@@ -18,11 +18,11 @@ use App\Http\Middleware\IsAdmin;
 */
 
 Route::post('/admin_login', [AdminUIController::class, 'adminLogin'])->name('adminLogin');
-Route::middleware('auth:api')->group(function () {
+// Route::middleware('auth:api')->group(function () {
     // Route::middleware([IsAdmin::class])->group(function () {
 
-        // Route::middleware([AdminAuth::class])->group(function(){
-        // Route::middleware([IsAdmin::class])->group(function(){
+Route::middleware([AdminAuth::class])->group(function(){
+    Route::middleware([IsAdmin::class])->group(function(){
         Route::post('/support', [AdminUIController::class, 'support'])->name('/support');
         Route::get('/dashboard', [AdminUIController::class, 'dashboardDetails'])->name('dashboard-details');
         Route::get('/setting', [AdminUIController::class, 'settingDetails'])->name('setting-details');
@@ -32,7 +32,7 @@ Route::middleware('auth:api')->group(function () {
         Route::get('/transactions', [AdminUIController::class, 'transactionList'])->name('transactionList');
         // Route::post('/transaction_list', [AdminUIController::class, 'transactionsList'])->name('transactionsList');
         Route::get('/user', [AdminUIController::class, 'user_list'])->name('user');
-    // });
+    });
 });
 
 Route::get('/', function () {
