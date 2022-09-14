9f3ee67e40c49909c0e147db844301b88f35807f
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-14 14:28:30 +0500

    Update OrderController.php

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -213,7 +213,6 @@ class OrderController extends Controller
         $datas['targetMSISDN'] = $targetMSISDN;
         $final['data'] = (object) $datas;
 
-        dd($final);
         // Topup API Request
         $accessToken = TopupToken::where('id', 1)->pluck('access_token')->first();
         $response = Http::withoutVerifying()->withHeaders([
