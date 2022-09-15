1bfa9aa5612de23657146c5d8406ea582b879bb1
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-15 18:18:04 +0500

    Update admin-navbar.blade.php

--- resources/views/includes/admin-navbar.blade.php
+++ resources/views/includes/admin-navbar.blade.php
@@ -303,6 +303,8 @@
                 $(notificationsArray).each(function (i, e) {
                   if(i % 2 === 0) {
                     $(".notification-profile").addClass('error-notification');
+                  }else{
+                    $(".notification-profile").removeClass('error-notification')
                   }
                   let div = `<a class="notification-area " href="#">
                     <div class="notification-profile d-flex py-3">
