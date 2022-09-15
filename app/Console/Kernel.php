055e139b00aef71ef81cb60c17e3c484d472dfc0
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-15 15:56:55 +0500

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('topup:cron')
-                ->everyMinute();
+                ->everyThirtyMinutes();
         
     }
 
