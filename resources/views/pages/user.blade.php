4fbf999214b651040b1421f15a4ea7a0426685d1
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-19 16:15:24 +0500

    Update user.blade.php

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -416,13 +416,12 @@
             date: lpurchase,
             phone_number: userphonenumber
         };
-        console.log(parameter);
         // Ajax call 
         $.ajax({
             url: 'http://kodextech.net/amin-topup/api/users',
             dataType: 'json', 
             type: 'POST',
-            data: JSON.stringify(parameter),
+            data: parameter,
             headers: {
                 'Authorization': 'Bearer ' + token,
                 'Content-Type' : 'application/json'
