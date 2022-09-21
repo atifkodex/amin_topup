409dd82fd5a9ddb6ce753fdbcb66c3f22087cc7c
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-21 12:10:32 +0500

    Update admin-default.blade.php

--- resources/views/layouts/admin-default.blade.php
+++ resources/views/layouts/admin-default.blade.php
@@ -7,11 +7,9 @@
     <meta name="csrf-token" content="{{ csrf_token() }}"/>
     <title>Amin Topup</title>
     <!-- Toaster CDN  -->
-    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0- 
-     alpha/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-	<link rel="stylesheet" type="text/css" 
-     href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
 
     @include('includes.admin-header')
