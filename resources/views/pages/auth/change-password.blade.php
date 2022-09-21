19424d615ef89fad0ab83f6cbbec40e2d05ecf06
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-21 12:19:47 +0500

    Update change-password.blade.php

--- resources/views/pages/auth/change-password.blade.php
+++ resources/views/pages/auth/change-password.blade.php
@@ -65,4 +65,43 @@
       }
     }
     </script>
+    <!-- Toaster Scripts Start-->
+<script>
+  @if(Session::has('message'))
+  toastr.options =
+  {
+  	"closeButton" : true,
+  	"progressBar" : true
+  }
+  		toastr.success("{{ session('message') }}");
+  @endif
+
+  @if(Session::has('error'))
+  toastr.options =
+  {
+  	"closeButton" : true,
+  	"progressBar" : true
+  }
+  		toastr.error("{{ session('error') }}");
+  @endif
+
+  @if(Session::has('info'))
+  toastr.options =
+  {
+  	"closeButton" : true,
+  	"progressBar" : true
+  }
+  		toastr.info("{{ session('info') }}");
+  @endif
+
+  @if(Session::has('warning'))
+  toastr.options =
+  {
+  	"closeButton" : true,
+  	"progressBar" : true
+  }
+  		toastr.warning("{{ session('warning') }}");
+  @endif
+</script>
+<!-- Toaster Scripts End  -->
 @endsection
