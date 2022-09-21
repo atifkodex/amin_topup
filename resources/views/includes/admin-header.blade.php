10a7a93dd0accde108e97e8995692a5aa7b9321e
Wahid Ahmad <wahid.a@kodextech.com>
2022-09-21 12:14:11 +0500

    update

--- resources/views/includes/admin-header.blade.php
+++ resources/views/includes/admin-header.blade.php
@@ -13,3 +13,6 @@
 <!-- ============== Custom Css Link ============== -->
 <link rel="stylesheet" href="{{asset('assets/css/zeeshan.css')}}">
 <link rel="stylesheet" href="{{asset('assets/css/tahira.css')}}">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+<script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
