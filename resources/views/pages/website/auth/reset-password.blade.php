4e0f355e2f8c6243048d16e1234897c0e9dea81e
Wahid Ahmad <wahid.a@kodextech.com>
2022-10-12 18:25:22 +0500

    Update reset-password.blade.php

--- resources/views/pages/website/auth/reset-password.blade.php
+++ resources/views/pages/website/auth/reset-password.blade.php
@@ -1,54 +1,63 @@
 @extends('layouts.website.default')
 
 <style>
-       .left-polygon-blue,.right-polygon-orange{
+    .left-polygon-blue,
+    .right-polygon-orange {
         display: none
     }
-    .inner-wrapper{
+
+    .inner-wrapper {
         position: relative;
     }
-    @media screen and (min-width:768px){
-    .left-polygon-blue{
-        position: absolute;
-        top:55% !important;
-        left: -2% !important;
-        width:8%;
-        /* z-index: -1; */
-        display: block;
-    }
-    .right-polygon-orange{
-        position: absolute;
-        top:64%;
-        right: 0px;
-        width: 8%;
-        /* z-index: -1; */
-        display: block;
-    }
+
+    @media screen and (min-width:768px) {
+        .left-polygon-blue {
+            position: absolute;
+            top: 55% !important;
+            left: -2% !important;
+            width: 8%;
+            /* z-index: -1; */
+            display: block;
+        }
+
+        .right-polygon-orange {
+            position: absolute;
+            top: 64%;
+            right: 0px;
+            width: 8%;
+            /* z-index: -1; */
+            display: block;
+        }
 
     }
+
     .reg-box {
         background: linear-gradient(73.58deg, #04376D 1.64%, #2762A1 88.81%) !important;
         border-radius: 16px;
     }
-    .reg-box p{
+
+    .reg-box p {
         color: white !important;
     }
-    .reg-box p a{
+
+    .reg-box p a {
         background: transparent !important;
     }
-    
+
     .right-inner {
         position: relative;
-    
+
     }
-    .right-inner  img{
-          position: absolute;
-          right: 0px;
-          padding: 12px 12px;
-          width: 50px;
-          z-index: 999;
-          top: 5px;
-        }
+
+    .right-inner img {
+        position: absolute;
+        right: 0px;
+        padding: 12px 12px;
+        width: 50px;
+        z-index: 999;
+        top: 5px;
+    }
+
     .info-section-two {
         position: relative;
     }
@@ -77,19 +86,20 @@
 
     }
 
-   
+
 
     @media screen and (max-width:576px) {
-        .right-inner  img{
-          position: absolute;
-          right: 0px;
-          padding: 12px 12px;
-          width: 40px;
-          z-index: 999;
-          top: 5px;
+        .right-inner img {
+            position: absolute;
+            right: 0px;
+            padding: 12px 12px;
+            width: 40px;
+            z-index: 999;
+            top: 5px;
         }
     }
-    .resetPasswordBtn{
+
+    .resetPasswordBtn {
         text-decoration: none !important;
         background-color: #001933 !important;
         color: white !important;
@@ -99,11 +109,10 @@
         font-weight: bold !important;
         outline: none !important;
     }
-
 </style>
 @section('content')
-    @include('includes.website.navbar')
-    <!-- <div class="container-fluid  px-0 outer-wrapper">
+@include('includes.website.navbar')
+<!-- <div class="container-fluid  px-0 outer-wrapper">
         <div class="inner-wrapper inner-wrapper-main">
             <img class="star-icon" src="{{ asset('assets/website-images/star-icon.svg') }}" alt="image">
             <div class="detail-box-outer pt-5  px-3">
@@ -183,60 +192,61 @@
           </defs>
       </svg>
     <div class="info-section-two container-fluid px-0"> -->
-        <div class="amount-section container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="amount-heading col-12 col-md-6 px-0 mt-4">
-                        <h1>Reset Password<span> </span></h1>
-                    </div>
-                </div>
-                <div class="col-12">
-                    <div class="reg-box my-3 my-lg-5">
-                        <form class="pt-5" id="resetPasswordForm_d" action="">
-                            @error('password')
-                                <div class="alert alert-danger alert-dismissible fade show login-email-field" role="alert">
-                                    {{ $message }}
-                                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span>
-                                    </button>
-                                </div>
-                            @enderror
-                            @if (Session::has('message'))
-                                <div class="alert alert-danger alert-dismissible fade show login-email-field" role="alert">
-                                    {{ Session::get('message') }}
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <span aria-hidden="true">&times;</span>
-                                </button>
-                                </div>
-                            @endif
-                            <div class="form-group form-field right-inner">
-                                <label for="">New Password</label>
-                                <input type="password" class="form-control" placeholder="enter password">
-                            </div>
-                            <div class="form-group form-field right-inner">
-                                <label for="">Confirm Password</label>
-                                <input type="password" class="form-control" id="loginpassword"  placeholder="enter password">
-                                <input type="hidden" class="form-control" id="loginEmail_d" >
-                            </div>
-                            <button type="btn" class="resetPasswordBtn">Reset Password</button>
-                        </form>
-                    </div>
-                </div>
+<div class="amount-section container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="amount-heading col-12 col-md-6 px-0 mt-4">
+                <h1>Reset Password<span> </span></h1>
             </div>
-            <!-- <img class="lefts-polygon-two" src="{{ asset('assets/website-images/left-polygon-three.svg') }}" alt="image"> -->
-            <!-- <img class="right-polygon-two" src="{{ asset('assets/website-images/right-polygon-two.svg') }}" alt="image"> -->
+        </div>
+        <div class="col-12">
+            <div class="reg-box my-3 my-lg-5">
+                <form class="pt-5" id="resetPasswordForm_d" action="{{route('resetPassword')}}" method="POST">
+                    @csrf
+                    @error('password')
+                    <div class="alert alert-danger alert-dismissible fade show login-email-field" role="alert">
+                        {{ $message }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @enderror
+                    @if (Session::has('message'))
+                    <div class="alert alert-danger alert-dismissible fade show login-email-field" role="alert">
+                        {{ Session::get('message') }}
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    @endif
+                    <div class="form-group form-field right-inner">
+                        <label for="">New Password</label>
+                        <input type="password" class="form-control" placeholder="enter password">
+                    </div>
+                    <div class="form-group form-field right-inner">
+                        <label for="">Confirm Password</label>
+                        <input type="password" class="form-control" name="password" id="loginpassword" placeholder="enter password">
+                        <input type="hidden" name="email" class="form-control" id="loginEmail_d">
+                    </div>
+                    <button type="submit" class="resetPasswordBtn">Reset Password</button>
+                </form>
+            </div>
         </div>
     </div>
-    @include('includes.website.footer-navbar')
+    <!-- <img class="lefts-polygon-two" src="{{ asset('assets/website-images/left-polygon-three.svg') }}" alt="image"> -->
+    <!-- <img class="right-polygon-two" src="{{ asset('assets/website-images/right-polygon-two.svg') }}" alt="image"> -->
+</div>
+</div>
+@include('includes.website.footer-navbar')
 @endsection
 
 @section('insertjavascript')
 <script>
-    $(document).ready(function(){
+    $(document).ready(function() {
         let email = localStorage.getItem('otpEmail');
         $("#loginEmail_d").val(email);
         var token = sessionStorage.getItem('userLoginData');
-        $("#resetPasswordBtn").click(function(){
+        $("#resetPasswordBtn_d").click(function() {
             var mail = $("#loginEmail_d").val();
             var password = $("#loginpassword").val();
             // Ajax call  
@@ -256,7 +266,7 @@
                 success: function(response) {
                     window.location.href = 'https://amintopup.com/main_login';
                 },
-                error: function (jqXHR, exception) {
+                error: function(jqXHR, exception) {
                     alert("Something went wrong. Please try again later.");
                 }
             });
