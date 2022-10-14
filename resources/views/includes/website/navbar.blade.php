7a844da3146cd8d5659ea262e26c6f877a57e5d3
zeeshan <zeeshan.a@kodextech.net>
2022-10-14 05:23:53 -0700

    updated

--- resources/views/includes/website/navbar.blade.php
+++ resources/views/includes/website/navbar.blade.php
@@ -4,7 +4,7 @@
         <img src="{{asset('assets/website-images/hamburger.png')}}" alt="">
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul class="navbar-nav ml-auto">
+      <ul class="navbar-nav ml-auto d-md-flex align-items-md-center">
         <li class="nav-item dropdown ">
             <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
              Amin Topup
@@ -23,7 +23,7 @@
         <li class="nav-item login">
             <a class="nav-link" href="{{url('main_login')}}">Login</a>
           </li>
-          <li class="nav-item dropdown active d-none">
+          <li class="nav-item dropdown active ">
             <a class="nav-link dropdown-toggle d-flex align-items-center" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               <img class="navbar-profile" src="{{asset('assets/website-images/profile.jpg')}}" alt="image">
                <p class="profile-name">Amin</p> 
--- resources/views/pages/website/recevier.blade.php
+++ resources/views/pages/website/recevier.blade.php
@@ -2,29 +2,54 @@
 <link rel="stylesheet" href="{{ asset('assets/css/slick.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/slick-theme.css') }}">
 <style>
-        .left-polygon-blue,.right-polygon-orange{
+     .lefts-polygon-two,.left-polygon-blue,.right-polygon-orange,.rights-polygon-two  {
         display: none
     }
-    .inner-wrapper{
-        position: relative;
-    }
     @media screen and (min-width:768px){
+        .lefts-polygon-two {
+        position: absolute;
+        bottom: 0% !important;
+        left: 0% !important;
+        width: 13%;
+        z-index: -1;
+        display: block;
+    }
+    .rights-polygon-two{
+        position: absolute;
+        bottom: 0% !important;
+        right: 16px;
+        width: 11% !important;
+        z-index: 1;
+        display: block;
+    }
     .left-polygon-blue{
         position: absolute;
-        top:55% !important;
-        left: -2% !important;
+        top: 8% !important;
+        left: 0% !important;
         width:8%;
         /* z-index: -1; */
         display: block;
     }
     .right-polygon-orange{
         position: absolute;
-        top:64%;
-        right: 0px;
+        top: 8px;
+        right: 16px;
         width: 8%;
         /* z-index: -1; */
         display: block;
     }
+
+    }
+    .amount-section{
+        background: #F0F7FF;
+border-radius: 16px;
+    }
+
+    .inner-wrapper{
+        position: relative;
+    }
+    @media screen and (min-width:768px){
+  
     .inner-wrapper-main{
         padding-bottom: 80px !important;
     }
@@ -77,36 +102,59 @@
 </style>
 @section('content')
     @include('includes.website.navbar')
-    <div class="container-fluid  px-0 outer-wrapper">
-        <div class="inner-wrapper inner-wrapper-main">
+    <div class="container-fluid outer-wrapper">
+        <div class="inner-wrapper">
             <img class="star-icon" src="{{ asset('assets/website-images/star-icon.svg') }}" alt="image">
-            <div class="detail-box-outer pt-5  px-3 ">
-                <div class="detail-box amount-box">
-                    <div class="main-form-section-content pb-4">
-                        <h1>Your <span>Details</span></h1>
-                    </div>
-                    <div class="network-list mb-1 d-flex align-items-center pb-3">
-                        <div class="network-icon">
+            <div class="inner-wrapper-heading container">
+                <h1>Your</h1>
+                <h1>Details</h1>
+            </div>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+                <path fill="#ffffff" fill-opacity="1"
+                    d="M0,32L48,58.7C96,85,192,139,288,176C384,213,480,235,576,202.7C672,171,768,85,864,85.3C960,85,1056,171,1152,197.3C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+                </path>
+            </svg>
+        </div>
+      
+    </div>
+    <svg style="visibility: hidden; position: absolute;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
+      <defs>
+            <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
+                <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9" result="goo" />
+                <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
+            </filter>
+        </defs>
+    </svg>
+      <div class="info-section-two container-fluid px-0 ">
+        <div class="amount-section container mb-5">
+        <div class="pt-5">
+                <div class="detail-box amount-box my-3">
+                  
+                    <div class="network-list mb-1 d-flex align-items-center justify-content-between pb-3">
+                        <div class="network-icon network-item text-left">
                             <img class="network-icon-image" src="{{ asset('assets/website-images/afg-flag.svg') }}"
                                 alt="image">
                         </div>
-                        <div class="network-text d-flex">
-                            <h1>93 70 00 00 000</h1>
+                        <div class="network-text network-item amin-gmail">
+                            <h1>93700000000</h1>
     
                         </div>
-                        <div class="network-button pl-2 pl-lg-5">
+                        <div class="network-button network-item text-right">
                             <button data-toggle="modal" data-target="#email-modal"><img class="edit-icon"  src="{{ asset('assets/website-images/edit-icon.svg') }}"
                                     alt="image"></button>
                         </div>
     
                     </div>
-                    <div class="network-list mb-1 d-flex align-items-center pb-3">
-                        <div class="network-icon">
+                    <div class="network-list mb-1 d-flex align-items-center pb-3 justify-content-between">
+                        <div class="network-icon network-item text-left">
                             <img class="network-icon-image" src="{{ asset('assets/website-images/awcc.svg') }}" alt="image">
                         </div>
-                        <div class="network-text d-flex">
+                        <div class="network-text network-item amin-gmail">
                             <h1>AWCC</h1>
     
+                        </div>
+                        <div class="network-button network-item invisible text-right">
+                       
                         </div>
                     </div>
        
@@ -115,28 +163,6 @@
                 </div>
            
             </div>
-       
-            
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                <path fill="#ffffff" fill-opacity="1"
-                    d="M0,32L48,58.7C96,85,192,139,288,176C384,213,480,235,576,202.7C672,171,768,85,864,85.3C960,85,1056,171,1152,197.3C1248,224,1344,192,1392,176L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
-                </path>
-            </svg>
-            
-        </div>
-        <img class="left-polygon-blue" src="{{ asset('assets/website-images/left-polygon-blue.svg') }}" alt="image">
-        <img class="right-polygon-orange" src="{{ asset('assets/website-images/right-polygon-orange.svg') }}" alt="image">
-    </div>
-    <svg style="visibility: hidden; position: absolute;" width="0" height="0" xmlns="http://www.w3.org/2000/svg" version="1.1">
-        <defs>
-              <filter id="goo"><feGaussianBlur in="SourceGraphic" stdDeviation="8" result="blur" />    
-                  <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 39 -9" result="goo" />
-                  <feComposite in="SourceGraphic" in2="goo" operator="atop"/>
-              </filter>
-          </defs>
-      </svg>
-      <div class="info-section-two container-fluid px-0 ">
-        <div class="amount-section container">
             <div class="row">
                 <div class="col-12">
                     <div class="reg-box mb-4 mb-lg-5">
@@ -159,6 +185,10 @@
                     </div>
                 </div>
             </div>
+            <img class="left-polygon-blue" src="{{ asset('assets/website-images/left-polygon-blue.svg') }}" alt="image">
+        <img class="right-polygon-orange" src="{{ asset('assets/website-images/right-polygon-orange.svg') }}" alt="image">
+        <img class="lefts-polygon-two" src="{{ asset('assets/website-images/left-polygon-three.svg') }}" alt="image">
+        <img class="rights-polygon-two" src="{{ asset('assets/website-images/right-polygon-two.svg') }}" alt="image">
         </div>
     </div>
     <div class="modal fade" id="email-modal" tabindex="-1" role="dialog" aria-labelledby="basicModal"
